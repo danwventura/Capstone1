a6ef7a75c14f50e6fc96b1b70e1441896e47d97a
Dan Ventura <danwventura@icloud.com>
2016-06-15 20:28:05 -0500

    registering new users and authenticating

--- app/app.js
+++ app/app.js
@@ -1,29 +1,44 @@
 var app = angular.module("iCaddy", ['ngRoute'])
 
 // constant for firebase here
+.constant("firebaseURL", "https://icaddy.firebaseio.com/")
 //constant for weather api here
+
+let isAuth = (AuthFactory) => new Promise ((resolve, reject) => {
+  if (AuthFactory.isAuthenticated ()){
+    resolve();
+  } else {
+    reject();
+  }
+})
+
 
 app.config(function($routeProvider){
   $routeProvider.
   when('/',{
   templateUrl: 'partials/main.html',
-  controller: 'MainViewCtrl'
+  controller: 'MainViewCtrl',
+  resolve: {isAuth}
   }).
   when('/weather',{
   templateUrl: 'partials/weather.html',
-  controller: 'WeatherCtrl'
+  controller: 'WeatherCtrl',
+  resolve: {isAuth}
   }).
   when('/scorecards/new',{
   templateUrl: 'partials/newround.html',
-  controller: 'NewCardCtrl'
+  controller: 'NewCardCtrl',
+  resolve: {isAuth}
   }).
   when('/scorecards/all',{
   templateUrl: 'partials/allCards.html',
-  controller: 'AllCardsCtrl'
+  controller: 'AllCardsCtrl',
+  resolve: {isAuth}
   }).
   when('/stats',{
   templateUrl: 'partials/stats.html',
-  controller: 'StatsCtrl'
+  controller: 'StatsCtrl',
+  resolve: {isAuth}
   }).
   when('/login',{
   templateUrl: 'partials/login.html',
@@ -35,3 +50,14 @@ app.config(function($routeProvider){
   }).
   otherwise('/');
   });
+
+
+app.run(($location) => {
+    let icaddyRef = new Firebase("https://icaddy.firebaseio.com/");
+
+    icaddyRef.onAuth(authData => {
+      if(!authData){
+        $location.path("/login")
+      }
+    })
+  })
