53a86a646511d727903f870a6f660773e3652a32
Dan Ventura <danwventura@icloud.com>
2016-06-22 21:20:43 -0500

    removed info

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ var app = angular.module("iCaddy", ['ngRoute'])
 // constant for firebase here
 .constant("firebaseURL", "https://icaddy.firebaseio.com/")
 //constant for weather api here
-.constant("weatherURL", "http://api.wunderground.com/api/24681701abdac30f/hourly/q/")
+.constant("weatherURL", "http://api.wunderground.com/api//hourly/q/")
 
 let isAuth = (AuthFactory) => new Promise ((resolve, reject) => {
   if (AuthFactory.isAuthenticated ()){
